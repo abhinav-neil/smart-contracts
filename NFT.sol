967a58c494591aa6c1cd3507977069f049c7d62e
abhinav-neil <abhinav.neil@gmail.com>
2022-02-14 23:37:44 +0100

    replaces ERC721Enumerable with ERC721, removes constructor args

--- NFT.sol
+++ NFT.sol
@@ -1,97 +1,51 @@
+// @notice NFT contract using ERC721 with Counters instead of ERC721Enumerable, to to optimize on gas for txns 
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.11;
 
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract NFT is ERC721, Ownable {
   using Strings for uint;
+  using Counters for Counters.Counter;
 
   string public baseURI;
   string public baseExtension;
   uint public price = 0.01 ether;
   uint public maxSupply = 10000;
-  uint public maxMintAmount = 3;
-  uint public maxTokensOfOwner = 10;
+  uint public maxTokensPerAddress = 10;
   bool public saleIsActive;
+  Counters.Counter private _tokenId;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI,
-    uint _initSupply
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-    for (uint i = 1; i <= _initSupply; i++) {
-      _safeMint(msg.sender, totalSupply() + 1);
-    }
-  }
+  constructor() ERC721("NFT", "NFT") {}
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-  
   function mint(uint _mintAmount) public payable {
-    uint supply = totalSupply();
-    require(saleIsActive, 'Sale is not active');
-    require(_mintAmount > 0, 'You must mint at least 1 NFT');
-    require(_mintAmount <= maxMintAmount, 'Max mint amount limit exceeded');
-    require(supply + _mintAmount <= maxSupply, 'Not enough supply');
-    require(balanceOf(msg.sender) + _mintAmount <= maxTokensOfOwner, 'Max tokens per address limit exceeded');
-    if (msg.sender != owner()) {
-        require(msg.value >= price * _mintAmount, 'Please send the correct amount of ETH');
-    }
-    for (uint i = 1; i <= _mintAmount; i++) {
-      _safeMint(msg.sender, supply + i);
+    require(saleIsActive, "Sale is not active");
+    require(_mintAmount > 0, "You must mint at least 1 NFT");
+    require(_tokenId.current() + _mintAmount < maxSupply, "Not enough supply");
+    require(balanceOf(msg.sender) + _mintAmount <= maxTokensPerAddress, "Max tokens per address limit exceeded");
+    require(msg.value >= price * _mintAmount, "Please send the correct amount of ETH");
+    for (uint i = 0; i < _mintAmount; i++) {
+        _tokenId.increment();
+        _safeMint(msg.sender, _tokenId.current());
     }
   }
 
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint[] memory)
-  {
-    uint ownerTokenCount = balanceOf(_owner);
-    uint[] memory tokenIds = new uint[](ownerTokenCount);
-    for (uint i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
-    }
-    return tokenIds;
+function tokenURI(uint tokenId) public view virtual override returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    return bytes(baseURI).length > 0
+        ? string(abi.encodePacked(baseURI, tokenId.toString(), baseExtension)): "";
   }
 
-  function tokenURI(uint tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      'ERC721Metadata: URI query for nonexistent token'
-    );
-
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : '';
-  }
-  
   function setPrice(uint _newPrice) public onlyOwner() {
     price = _newPrice;
   }
 
-  function setmaxMintAmount(uint _newmaxMintAmount) public onlyOwner() {
-    maxMintAmount = _newmaxMintAmount;
-  }
-
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
-
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
+  function setBaseURI(string memory _baseURI, string memory _baseExtension) public onlyOwner {
+    baseURI = _baseURI;
+    baseExtension = _baseExtension;
   }
 
   function flipSaleState() public onlyOwner {
@@ -99,7 +53,7 @@ contract NFT is ERC721Enumerable, Ownable {
   }
  
   function withdraw() public payable onlyOwner {
-    (bool success, ) = payable(msg.sender).call{value: address(this).balance}('');
-    require(success);
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(success, "Withdrawal of funds failed");
   }
 }
